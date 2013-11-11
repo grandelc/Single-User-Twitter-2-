58dcc5de262f75337342e8bc554bf6874045de63
Grandel Cuvos <graandelc@gmail.com>
2013-11-10 21:29:48 -0600

    Added a validation for lenght of tweet status.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,10 @@
 class Tweet < ActiveRecord::Base
   # Associated with tweets table in the db.
   attr_accessible :status
+
+  # Old Style
+  validates :status, :length => { :maximum => 140, :minimum => 1 }
+
+  # New Style
+  # validates :status, length: => { maximum: 140, minimum: 1 }
 end
